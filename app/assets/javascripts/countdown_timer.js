e63ccb998b553df8cb54f66e0871d0ba287402d4
Juan Andrés Diana <juandiana@gmail.com>
2016-06-22 22:13:11 -0300

    Added the countdown timer.

---
+++ app/assets/javascripts/countdown_timer.js
@@ -0,0 +1,43 @@
+
+function getTimeRemaining(endtime) {
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
+}
+
+function initializeClock(id, endtime) {
+    var clock = $('#' + id);
+    var daysSpan = clock.find('#days');
+    var hoursSpan = clock.find('#hours');
+    var minutesSpan = clock.find('#mins');
+    var secondsSpan = clock.find('#secs');
+
+    function updateClock() {
+        var t = getTimeRemaining(endtime);
+
+        daysSpan.html(t.days);
+        hoursSpan.html(('0' + t.hours).slice(-2));
+        minutesSpan.html(('0' + t.minutes).slice(-2));
+        secondsSpan.html(('0' + t.seconds).slice(-2));
+
+        if (t.total <= 0) {
+            clearInterval(timeinterval);
+        }
+    }
+
+    updateClock();
+    var timeinterval = setInterval(updateClock, 1000);
+}
+
+
+var deadline = 'November 12 2016 09:00:00 GMT-0300';
+initializeClock('countdown_timer', deadline);

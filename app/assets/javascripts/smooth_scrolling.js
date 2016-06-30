0350af9eacbf81b19b0c40955c7c8eee54f6a626
Juan Andrés Diana <juandiana@gmail.com>
2016-06-30 01:10:40 -0300

    Add smooth scrolling to internal anchors.

---
+++ app/assets/javascripts/smooth_scrolling.js
@@ -0,0 +1,18 @@
+/**
+ *  All internal anchor links will have a 1 sec smooth scrolling animation to their destination, and the URL will be
+ *  updated to reflect the new position on the page.
+ */
+$(document).ready(function() {
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+});

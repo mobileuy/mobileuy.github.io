b7f30ccf69dd5d3a76e8eff948ae6d72ec07d8e3
Damián Arrillaga <damian.arrillaga@gmail.com>
2016-07-10 00:18:45 -0300

    Fix particles on safari, working on venue/footer

--- app/assets/javascripts/json_content_utils.js
+++ app/assets/javascripts/json_content_utils.js
@@ -7,25 +7,36 @@ function removeQuotes(string) {
 
 function getBreakpoint(element) {
     var style = null;
-    if ( window.getComputedStyle && window.getComputedStyle(element, '::before') ) {
+    if ( window.getComputedStyle && window.getComputedStyle(element, '::before') && window.getComputedStyle(element, '::before').content != "" ) {
         style = window.getComputedStyle(element, '::before');
         style = style.content;
     } else {
-        window.getComputedStyle = function(el) {
-            this.el = el;
-            this.getPropertyValue = function(prop) {
-                var re = /(\-([a-z]){1})/g;
-                if (re.test(prop)) {
-                    prop = prop.replace(re, function () {
-                        return arguments[2].toUpperCase();
-                    });
+        if (typeof window.getComputedStyle === "undefined") {
+            window.getComputedStyle = function (el) {
+                this.el = el;
+                this.getPropertyValue = function (prop) {
+
+                    var style = el.currentStyle || el.style
+
+                    if (typeof prop === "undefined" || typeof style === "undefined") {
+                        return null;
+                    }
+
+                    var re = /(\-([a-z]){1})/g;
+                    if (re.test(prop)) {
+                        prop = prop.replace(re, function () {
+                            return arguments[2].toUpperCase();
+                        });
+                    }
+                    return style[prop] ? style[prop] : null;
                 }
-                return el.currentStyle[prop] ? el.currentStyle[prop] : null;
-            };
-            return this;
-        };
+                return this;
+            }
+        }
         style = window.getComputedStyle(document.getElementsByTagName('head')[0]);
         style = style.getPropertyValue('font-family');
     }
+
+    style = style || "{}";
     return JSON.parse(removeQuotes(style));
 }

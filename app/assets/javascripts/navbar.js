2c56cc0eca8b37e8e55a02d4a43a8dad3134d528
Damián Arrillaga <damian.arrillaga@gmail.com>
2016-08-21 18:10:08 -0300

    Fix scroll on mobile navbar and collapse on menu click

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -2,10 +2,20 @@ $(document.body).on('click', '.navbar-toggle.collapsed', function () {
   $(this).removeClass("collapsed");
   var targetNav = $(this).attr("data-target");
   $(targetNav).addClass("in");
+  $('html').addClass('nav-bar-opened');
+
+  $(targetNav).on('click', '.c-menu__item', function() {
+    collapse(targetNav);
+  });
 });
 
 $(document.body).on('click', '.navbar-toggle:not(.collapsed)', function () {
-  var targetNav = $(this).attr("data-target");
-  $(targetNav).removeClass("in");
-  $(this).addClass("collapsed");
+  collapse($(this).attr("data-target"));
 });
+
+function collapse(targetNav) {
+  $(targetNav).removeClass("in");
+  $('.navbar-toggle').addClass("collapsed");
+  $('html').removeClass('nav-bar-opened');
+  $(targetNav).off('click', '.c-menu__item');
+}

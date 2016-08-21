9347d8706ac6d2f84ce0743d5393e20a42a17ad3
Juan Andrés Diana <juandiana@gmail.com>
2016-08-21 02:04:52 -0300

    Map Fixes: - Use styles/mobiledayuy for the style. - Use a non-interactive, 0 pitch map view. - Use a custom image for the marker (blue-marker). - Conditionally center the map depending on the viewport breakpoint we are in.

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,17 +1,41 @@
 $(function(){
-  mapboxgl.accessToken = 'pk.eyJ1IjoiaW1uaWNvc3VhcmV6IiwiYSI6ImNpbWtzcXJ6MDAxNXd1cWt1Y2s4bjhlbWsifQ.sJHlwuteFm58lmBRkboFLg';
-  var map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/imnicosuarez/cimksxx8h00bcg4ns8s2k9svz',
-      zoom: 15.83,
-      center: [-56.200090, -34.893585],
-      pitch: 30.00,
-      scrollZoom: false,
-      dragPan: false
-  });
+    mapboxgl.accessToken = 'pk.eyJ1IjoibW9iaWxlZGF5dXkiLCJhIjoiY2lxNHNhN2QyMDAwYmZsbTZydG4yb2ZzeSJ9.lFAWW3TvMm5EzUOQoNPloQ';
 
-  var marker = new mapboxgl.Marker()
-  .setLngLat([-56.200090, -34.893585])
-  .addTo(map);
+    var isMobileViewport = getBreakpoint(document.querySelector('.variables-metadata')).mobile_viewport;
+    var venuePos = [-56.19448037405206, -34.89219928156295];
+    var centerPos = isMobileViewport ? venuePos : [-56.201700, -34.892280];
 
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mobiledayuy/cirmxjudm000lg3nkghetgp16',
+        zoom: 15.26,
+        center: centerPos,
+        pitch: 0.00,
+        interactive: false
+    });
+
+    map.on('style.load', function () {
+        map.addSource("markers", {
+            "type": "geojson",
+            "data": {
+                "type": "FeatureCollection",
+                "features": [{
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [-56.19448037405206, -34.89219928156295]
+                    }
+                }]
+            }
+        });
+        map.addLayer({
+            "id": "markers",
+            "type": "symbol",
+            "source": "markers",
+            "layout": {
+                "icon-image": "blue-marker",
+                "icon-offset": [0, -30]
+            }
+        });
+    });
 });
